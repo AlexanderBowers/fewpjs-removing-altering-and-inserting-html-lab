ec465fef788f9b68534ced9dd958e605d78c62b7
AlexanderBowers <7319abowers@gmail.com>
2021-01-12 09:41:37 -0800

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+let newElement = document.body.querySelector("main#main");
+
+newElement.remove("main#main");
+
+let h1 = document.createElement('h1');
+h1.id = "victory";
+h1.innerHTML = `Alex is the champion`;
+document.body.appendChild(h1);
+
+let newHeader = document.querySelector("h1#victory");
+
